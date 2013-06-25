dec6438d4c3880d7b3d08f2a1f9fe8ed00f0c6d4
Jer Lance <me@jerlance.com>
2013-06-24 21:04:29 -0400

    Fixing script file

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
+CURRPATH=`pwd`
+
 if [ -f "$HOME/.bash_aliases" ]; then
     mv "$HOME/.bash_aliases" "$HOME/.bash_aliases.old"
 fi
-ln -s .bash_aliases "$HOME/.bash_aliases"
+ln -s "$CURRPATH/.bash_aliases" "$HOME/.bash_aliases"
 
 
 if [ -f "$HOME/.bash_profile" ]; then
     mv "$HOME/.bash_profile" "$HOME/.bash_profile.old"
 fi
-ln -s .bash_profile "$HOME/.bash_profile"
+ln -s "$CURRPATH/.bash_profile" "$HOME/.bash_profile"
 
 if [ -f "$HOME/.bash_prompt" ]; then
     mv "$HOME/.bash_prompt" "$HOME/.bash_prompt.old"
 fi
-ln -s .bash_prompt "$HOME/.bash_prompt"
+ln -s "$CURRPATH/.bash_prompt" "$HOME/.bash_prompt"
 
 if [ -d "$HOME/.vim" ]; then
     mv "$HOME/.vim" "$HOME/.vim.old"
 fi
-ln -s .vim "$HOME/.vim"
+ln -s "$CURRPATH/.vim" "$HOME/.vim"
 
 if [ -f "$HOME/.vimrc" ]; then
     mv "$HOME/.vimrc" "$HOME/.vimrc.old"
 fi
-ln -s .vimrc "$HOME/.vimrc"
+ln -s "$CURRPATH/.vimrc" "$HOME/.vimrc"
